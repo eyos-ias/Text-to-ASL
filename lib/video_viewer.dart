5e336fa289987e880705cc46d1aa98373e77962c
Firaol Tsegaye <125078028+FiraolTsegaye@users.noreply.github.com>
2023-09-07 16:53:38 +0300

    video input page

--- lib/video_viewer.dart
+++ lib/video_viewer.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:video_player/video_player.dart';
-import 'dart:io';
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Video Player',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: VideoPage(),
+    );
+  }
+}
 
 class VideoPage extends StatefulWidget {
   @override
@@ -12,22 +28,25 @@ class VideoPage extends StatefulWidget {
 class _VideoPageState extends State<VideoPage> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
-  late VideoPlayerController _videoPlayerController;
+  late VideoPlayerController _videoPlayerController =
+      VideoPlayerController.asset('assets/placeholder_video.mp4');
 
   List<CameraDescription> cameras = [];
+  bool showCameraPreview = false;
+  bool isRecording = false;
+  int recordingTime = 0;
+  late Timer _timer;
 
   @override
   void initState() {
     super.initState();
-    // Retrieve the available cameras
     availableCameras().then((availableCameras) {
       cameras = availableCameras;
-      // Initialize the camera controller
       _controller = CameraController(
         cameras[0],
         ResolutionPreset.medium,
+        enableAudio: false,
       );
-      // Initialize the camera controller future
       _initializeControllerFuture = _controller.initialize();
       setState(() {});
     });
@@ -35,23 +54,43 @@ class _VideoPageState extends State<VideoPage> {
 
   @override
   void dispose() {
-    // Dispose of the camera controller and video player controller
     _controller.dispose();
     _videoPlayerController.dispose();
+    _timer.cancel();
     super.dispose();
   }
 
-  Future<void> _selectVideoFromStorage() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.video,
-    );
+  Future<void> _captureVideo() async {
+    if (!_controller.value.isInitialized || !showCameraPreview) {
+      return;
+    }
 
-    if (result != null) {
-      File videoFile = File(result.files.first.path!);
-      _videoPlayerController = VideoPlayerController.file(videoFile)
-        ..initialize().then((_) {
-          setState(() {});
+    try {
+      await _initializeControllerFuture;
+      if (!_controller.value.isRecordingVideo) {
+        setState(() {
+          isRecording = true;
+          recordingTime = 0;
         });
+
+        _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+          setState(() {
+            recordingTime++;
+          });
+        });
+
+        await _controller.startVideoRecording();
+        print('Video recording started');
+      } else {
+        _timer.cancel();
+        await _controller.stopVideoRecording();
+        setState(() {
+          isRecording = false;
+        });
+        print('Video recording stopped');
+      }
+    } catch (e) {
+      print('Error capturing video: $e');
     }
   }
 
@@ -59,11 +98,63 @@ class _VideoPageState extends State<VideoPage> {
     if (_videoPlayerController.value.isInitialized) {
       return AspectRatio(
         aspectRatio: _videoPlayerController.value.aspectRatio,
-        child: VideoPlayer(_videoPlayerController),
+        child: Stack(
+          children: [
+            VideoPlayer(_videoPlayerController),
+            Positioned(
+              top: showCameraPreview
+                  ? _controller.value.previewSize!.height + 10
+                  : 0,
+              left: 16,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Text(
+                  'Recording Time: ${_formatRecordingTime(recordingTime)}',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            if (isRecording)
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Recording in progress',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      );
+    } else if (_videoPlayerController.value.hasError) {
+      return Text(
+        'Error loading video',
+        style: TextStyle(color: Colors.red),
       );
     } else {
-      return CircularProgressIndicator();
+      return Container();
     }
+  }
+
+  String _formatRecordingTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int remainingSeconds = seconds % 60;
+    String formattedTime =
+        '${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}';
+    return formattedTime;
   }
 
   @override
@@ -94,7 +185,7 @@ class _VideoPageState extends State<VideoPage> {
                 ),
                 SizedBox(width: 8),
                 Text(
-                  'Video',
+                  'Video Input',
                   style: TextStyle(color: Colors.white),
                 ),
               ],
@@ -103,23 +194,22 @@ class _VideoPageState extends State<VideoPage> {
         ),
         body: Column(
           children: [
-            Expanded(
-              child: FutureBuilder<void>(
-                future: _initializeControllerFuture,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    // If the Future is complete, display the camera preview
-                    return CameraPreview(_controller);
-                  } else {
-                    // Otherwise, display a loading spinner
-                    return Center(child: CircularProgressIndicator());
-                  }
-                },
+            if (showCameraPreview)
+              Expanded(
+                child: FutureBuilder<void>(
+                  future: _initializeControllerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return CameraPreview(_controller);
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
               ),
-            ),
             ElevatedButton(
-              onPressed: _selectVideoFromStorage,
-              child: Text('Select Video from Storage'),
+              onPressed: _captureVideo,
+              child: Text(isRecording ? 'Stop Recording' : 'Start Recording'),
             ),
             SizedBox(height: 16),
             _buildVideoPlayer(),
